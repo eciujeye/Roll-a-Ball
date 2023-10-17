88b91c1b56a6ed2e7fd8d7427ff8b60647f601df
eciujeye <claireoh2004@gmail.com>
2023-10-16 20:12:53 -0700

    Updated for jumping and speed limit

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -13,6 +13,10 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     public float speed = 0;
     public TextMeshProUGUI countText;
+    private bool onGround = true;
+    //public float raycastDistance = 0.6f;
+
+    public float maxSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -49,10 +53,59 @@ public class PlayerController : MonoBehaviour
        {
           SetCountText();
           other.gameObject.SetActive(false);
-       }
+       } 
 
-     
+     if(other.gameObject.CompareTag("foot")) {
+          Debug.Log("On Ground");
+          onGround = true; 
+     } 
    }
+
+   void OnCollisionEnter (Collision other) {
+     //if (other.gameObject.CompareTag("foot")) {
+          Debug.Log("2");
+          onGround = true;
+
+     //}
+   }
+
+   void OnCollisionExit (Collision other) {
+     //if (other.gameObject.CompareTag("foot")) {
+          Debug.Log("1");
+          onGround = false; 
+    // }
+   }
+
+
+
+     void OnJump() {
+          
+          //rb.velcoity = new Vector3(0,5,0);
+          if(onGround) {
+              Debug.Log("Jumped");
+              rb.velocity = new Vector3(0,10,0);
+          } else {
+               Debug.Log("Nah");
+          }
+
+     }
+
+     private void Update() {
+     //      RaycastHit hit;
+     //      if(Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance, groundLayer)) {
+     //           isGrounded = true;
+     //           Debug.Log("raycast1");
+     //      } else {
+     //           isGrounded = false; 
+     //           Debug.Log("raycast2");
+     //      }
+
+          Debug.Log(rb.velocity.magnitude);
+         if(rb.velocity.magnitude > maxSpeed) {
+               rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+         }      
+
+     }
    
 
 }
